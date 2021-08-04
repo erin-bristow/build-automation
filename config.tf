7049d9136bf905c10e966a09148ccf2b396cbe02
Julie <julie.howard.1987@protonmail.com>
2021-08-05 02:23:35 +0530

    Deployment pending

---
+++ config.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8080
+  }
+  volumes {
+    container_path = "/usr/share/nginx/html"
+    host_path      = "/Users/kunal/Projects/reconvillage/challenge 1/www"
+    read_only      = true
+  }
+}
+
+// remember to push to registry my-image:latest
